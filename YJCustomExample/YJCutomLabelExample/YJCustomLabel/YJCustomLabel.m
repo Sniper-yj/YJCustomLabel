d7709764dde289e261e48b914da57bfd517b34f5
Sniper-yj <Sniper_YJ@sniperYJdeMacBook-Pro.local>
2015-08-11 17:11:56 +0800

    点击高亮功能

--- YJCustomExample/YJCutomLabelExample/YJCustomLabel/YJCustomLabel.m
+++ YJCustomExample/YJCutomLabelExample/YJCustomLabel/YJCustomLabel.m
@@ -20,6 +20,7 @@ static inline CGFLOAT_TYPE CGFloat_ceil(CGFLOAT_TYPE cgfloat) {
 @implementation YJCustomLabel
 {
     UIImageView *labelImageView;///<显示label内容的图片
+    UIImageView *HighlightLabelImageView;///<显示点击后的label内容图片
     NSInteger row;//行高
     NSMutableDictionary *framesDict;///<储存特殊位置的数组
     NSRange currentRange;///<当前的点击区域
@@ -41,13 +42,18 @@ CTTextAlignment CTTextAlignmentFromUITextAlignment(NSTextAlignment alignment) {
     self = [super initWithFrame:frame];
     if (self)
     {
-        //        currentRange = NULL;
         framesDict = [NSMutableDictionary dictionary];
         labelImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
         labelImageView.contentMode = UIViewContentModeScaleAspectFit;
         labelImageView.tag = NSIntegerMin;
         labelImageView.clipsToBounds = YES;
         [self addSubview:labelImageView];
+        
+        HighlightLabelImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
+        HighlightLabelImageView.contentMode = UIViewContentModeScaleAspectFit;
+        HighlightLabelImageView.tag = NSIntegerMin;
+        HighlightLabelImageView.clipsToBounds = YES;
+        [self addSubview:HighlightLabelImageView];
         
         self.userInteractionEnabled = YES;
         self.clipsToBounds = NO;
@@ -70,68 +76,81 @@ CTTextAlignment CTTextAlignmentFromUITextAlignment(NSTextAlignment alignment) {
     if (text == nil || text.length <= 0)
     {
         labelImageView.image = nil;
+        HighlightLabelImageView.image = nil;
         return;
     }
-    //    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-    NSString *textMessage = text;
-    _text = textMessage;
-    UIColor *textColor = self.textColor;
-    CGSize size = self.frame.size;
-    UIGraphicsBeginImageContextWithOptions(size, ![self.backgroundColor isEqual:[UIColor clearColor]], 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    if (context == NULL)
-    {
-        return;
-    }
-    if (![self.backgroundColor isEqual:[UIColor clearColor]]) {
-        [self.backgroundColor set];
-        CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
-    }
-    CGContextSetTextMatrix(context,CGAffineTransformIdentity);
-    CGContextTranslateCTM(context,0,size.height);
-    CGContextScaleCTM(context,1.0,-1.0);
-    CGMutablePathRef path = CGPathCreateMutable();
-    CGPathAddRect(path, NULL, self.bounds);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSString *textMessage = text;
+        _text = textMessage;
+        UIColor *textColor = self.textColor;
+        CGSize size = self.frame.size;
+        UIGraphicsBeginImageContextWithOptions(size, ![self.backgroundColor isEqual:[UIColor clearColor]], 0);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        if (context == NULL)
+        {
+            return;
+        }
+        if (![self.backgroundColor isEqual:[UIColor clearColor]]) {
+            [self.backgroundColor set];
+            CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
+        }
+        CGContextSetTextMatrix(context,CGAffineTransformIdentity);
+        CGContextTranslateCTM(context,0,size.height);
+        CGContextScaleCTM(context,1.0,-1.0);
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathAddRect(path, NULL, self.bounds);
+        
+        CGFloat minimumLineHeight = self.font.pointSize,maximumLineHeight = minimumLineHeight, linespace = self.lineSpace;
+        CGFloat fristLineHead = _headSpace;
+        CTLineBreakMode lineBreakMode = kCTLineBreakByCharWrapping;
+        CTTextAlignment alignment = CTTextAlignmentFromUITextAlignment(self.textAlignment);
+        CTFontRef font = CTFontCreateWithName((__bridge CFStringRef)self.font.fontName, self.font.pointSize, NULL);
+        CTParagraphStyleRef paragraphStyle = CTParagraphStyleCreate((CTParagraphStyleSetting[7]){{kCTParagraphStyleSpecifierAlignment,sizeof(alignment),&alignment},{kCTParagraphStyleSpecifierLineBreakMode,sizeof(lineBreakMode),&lineBreakMode},{kCTParagraphStyleSpecifierMaximumLineHeight,sizeof(maximumLineHeight),&maximumLineHeight},{kCTParagraphStyleSpecifierMinimumLineHeight,sizeof(minimumLineHeight),&minimumLineHeight},{kCTParagraphStyleSpecifierMaximumLineSpacing,sizeof(linespace),&linespace},
+            {kCTParagraphStyleSpecifierMinimumLineSpacing,sizeof(linespace),&linespace},{kCTParagraphStyleSpecifierFirstLineHeadIndent,sizeof(fristLineHead),&fristLineHead}}, 7);
+        
+        
+        //新增每个字的间距
+        CFNumberRef num = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &_wordSpace);
+        
+        
+        NSDictionary *attributeDic = [NSDictionary dictionaryWithObjectsAndKeys:(__bridge id)textColor.CGColor,kCTForegroundColorAttributeName,font,kCTFontAttributeName,paragraphStyle,kCTParagraphStyleAttributeName,num,kCTKernAttributeName, nil];
+        
+        NSMutableAttributedString *attributeString = [[NSMutableAttributedString alloc] initWithString:text attributes:attributeDic];
+        //        [attributeString addAttribute:(NSString *)kCTKernAttributeName value:(__bridge id)(num) range:NSMakeRange(0, 3)];
+        
+        NSMutableAttributedString *attributeHighString = [self highlightString:attributeString];
+        
+        CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attributeHighString);
+        
+        CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, text.length), path, NULL);
+        CGRect rect = CGRectMake(0, 0,(size.width),(size.height));
+        [self drawFramesetter:framesetter attributedString:attributeHighString textRange:CFRangeMake(0, _text.length) inRect:rect context:context];
+        
+        CTFrameDraw(frame, context);
+        UIImage *screenShotimage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        dispatch_async(dispatch_get_main_queue(), ^{
     
-    CGFloat minimumLineHeight = self.font.pointSize,maximumLineHeight = minimumLineHeight, linespace = self.lineSpace;
-    CGFloat fristLineHead = _headSpace;
-    CTLineBreakMode lineBreakMode = kCTLineBreakByCharWrapping;
-    CTTextAlignment alignment = CTTextAlignmentFromUITextAlignment(self.textAlignment);
-    CTFontRef font = CTFontCreateWithName((__bridge CFStringRef)self.font.fontName, self.font.pointSize, NULL);
-    CTParagraphStyleRef paragraphStyle = CTParagraphStyleCreate((CTParagraphStyleSetting[7]){{kCTParagraphStyleSpecifierAlignment,sizeof(alignment),&alignment},{kCTParagraphStyleSpecifierLineBreakMode,sizeof(lineBreakMode),&lineBreakMode},{kCTParagraphStyleSpecifierMaximumLineHeight,sizeof(maximumLineHeight),&maximumLineHeight},{kCTParagraphStyleSpecifierMinimumLineHeight,sizeof(minimumLineHeight),&minimumLineHeight},{kCTParagraphStyleSpecifierMaximumLineSpacing,sizeof(linespace),&linespace},
-        {kCTParagraphStyleSpecifierMinimumLineSpacing,sizeof(linespace),&linespace},{kCTParagraphStyleSpecifierFirstLineHeadIndent,sizeof(fristLineHead),&fristLineHead}}, 7);
-    
-    
-    //新增每个字的间距
-    CFNumberRef num = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt8Type, &_wordSpace);
-    
-    
-    NSDictionary *attributeDic = [NSDictionary dictionaryWithObjectsAndKeys:(__bridge id)textColor.CGColor,kCTForegroundColorAttributeName,font,kCTFontAttributeName,paragraphStyle,kCTParagraphStyleAttributeName,num,kCTKernAttributeName, nil];
-    
-    NSMutableAttributedString *attributeString = [[NSMutableAttributedString alloc] initWithString:text attributes:attributeDic];
-    //        [attributeString addAttribute:(NSString *)kCTKernAttributeName value:(__bridge id)(num) range:NSMakeRange(0, 3)];
-    
-    NSMutableAttributedString *attributeHighString = [self highlightString:attributeString];
-    
-    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attributeHighString);
-    
-    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, text.length), path, NULL);
-    CGRect rect = CGRectMake(0, 0,(size.width),(size.height));
-    [self drawFramesetter:framesetter attributedString:attributeHighString textRange:CFRangeMake(0, _text.length) inRect:rect context:context];
-    
-    CTFrameDraw(frame, context);
-    UIImage *screenShotimage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    //        dispatch_async(dispatch_get_main_queue(), ^{
-    
-    labelImageView.image = nil;
-    labelImageView.image = screenShotimage;
-    CFRelease(path);
-    CFRelease(frame);
-    CFRelease(framesetter);
-    CFRelease(paragraphStyle);
-    CFRelease(font);
-    CFRelease(num);
+            labelImageView.image = nil;
+            HighlightLabelImageView.image = nil;
+            
+            if (highlighting)
+            {
+                HighlightLabelImageView.image = screenShotimage;
+            }
+            else
+            {
+                labelImageView.image = screenShotimage;
+            }
+            
+            CFRelease(path);
+            CFRelease(frame);
+            CFRelease(framesetter);
+            CFRelease(paragraphStyle);
+            CFRelease(font);
+            CFRelease(num);
+        });
+    });
 }
 
 - (void)drawFramesetter:(CTFramesetterRef)framesetter
@@ -261,16 +280,28 @@ CTTextAlignment CTTextAlignmentFromUITextAlignment(NSTextAlignment alignment) {
             {
                 if (labelImageView.image != nil && currentRange.location!=-1 && currentRange.location>=match.range.location && currentRange.length+currentRange.location<=match.range.length+match.range.location)
                 {
+                    [attributString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)[UIColor redColor].CGColor range:match.range];
                     NSString *str = [attributString.string substringWithRange:match.range];
-                    if (self.clickBlock)
-                    {
-                        self.clickBlock(str);
-                    }
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        
+                        if (self.clickBlock)
+                        {
+                            self.clickBlock(str);
+                        }
+                    });
                 }
+                else
+                {
+                    [attributString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)[UIColor blueColor].CGColor range:match.range];
+                }
+            }
+            else
+            {
+                
+                [attributString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)[UIColor blueColor].CGColor range:match.range];
             }
             
             
-            [attributString addAttribute:(NSString *)kCTForegroundColorAttributeName value:(__bridge id)[UIColor blueColor].CGColor range:match.range];
         }
     }
     return attributString;
@@ -341,13 +372,20 @@ CTTextAlignment CTTextAlignmentFromUITextAlignment(NSTextAlignment alignment) {
         }
     }
 }
+
+
 -(void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    highlighting = NO;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        highlighting = NO;
+        [self setText:_text];
+    });
 }
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     highlighting = NO;
+    [self setText:_text];
 }
 
 @end
